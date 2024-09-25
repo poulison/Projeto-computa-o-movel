b1cb64de45a8a117c8a09a0b63d12862d3eadb79
Paulo Hirata <127874722+poulison@users.noreply.github.com>
2024-09-25 15:38:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -102,3 +102,140 @@ void loop() {
         contagemseq = 0; // Resetar contagemseq para cada nova tentativa
         bool erro = false;
         
+        for (int b = 0; b < tamanho; b++) {
+            lcd_1.clear();
+            lcd_1.print("Contagem: ");
+            lcd_1.print(contagemseq);
+
+            // Espera até que um botão seja pressionado
+            while (true) {
+                int botaovd = digitalRead(8);
+                int botaovm = digitalRead(10);
+                
+                if (botaovd == LOW) {
+                    indsequeusuario = 1;
+                    digitalWrite(verde, HIGH);
+                    delay(1000);
+                    digitalWrite(verde, LOW);
+                    memoriausu[b] = indsequeusuario;
+                    contagemseq++;
+                    break; // Sai do loop enquanto para processar a próxima sequência
+                } 
+                else if (botaovm == LOW) {
+                    indsequeusuario = 0;
+                    digitalWrite(vermelho, HIGH);
+                    delay(1000);
+                    digitalWrite(vermelho, LOW);
+                    memoriausu[b] = indsequeusuario;
+                    contagemseq++;
+                    break; // Sai do loop enquanto para processar a próxima sequência
+                }
+            }
+
+            // Verifica se a entrada do usuário está correta
+            if (memoriausu[b] != memoria[b]) {
+                lcd_1.clear();
+                lcd_1.print("Errou!");
+                delay(1000);
+                erro = true; // Marca que houve erro
+                break; // Sai do loop
+            }
+        }
+
+        if (!erro) {
+            lcd_1.clear();
+            lcd_1.print("Parabens!"); // Se tudo correto
+            delay(1000);
+        }
+        
+        lcd_1.clear();
+        lcd_1.print("segunda parte"); 
+        lcd_1.setCursor(0, 1);
+        lcd_1.print("perguntas");
+        delay(1000);
+        
+      // embaralhar os char de perguntas e respostas
+      	embaralharPerguntas();
+      	selecionarPerguntas();
+      
+      	
+      for(int i=0;i<numeroDePerguntas;i++){
+        segundos = 0;
+        
+        
+        
+        
+        if (segundos>10){
+        	lcd_1.clear();
+            lcd_1.print("acabou o tempo"); 
+            delay(1000);
+          	break;
+        
+        }
+      
+      
+      }
+        
+        lcd_1.clear(); // Limpa o LCD
+        lcd_1.print("Pressione Iniciar"); // Volta a mensagem inicial
+    }
+}
+
+
+void embaralharPerguntas() {
+    for (int i = tamanhoquest - 1; i > 0; i--) {
+        int j = random(i + 1); // Gera um índice aleatório
+        // Troca as perguntas
+        String tempPergunta = perguntas[i];
+        perguntas[i] = perguntas[j];
+        perguntas[j] = tempPergunta;
+
+        // Troca as respostas
+        String tempResposta = respostas[i];
+        respostas[i] = respostas[j];
+        respostas[j] = tempResposta;
+    }
+}
+
+void selecionarPerguntas() {
+    String perguntasSelecionadas[numeroDePerguntas];
+    String respostasSelecionadas[numeroDePerguntas];
+
+    for (int i = 0; i <= numeroDePerguntas; i++) {
+        perguntasSelecionadas[i] = perguntas[i]; // Pega as 5 primeiras após embaralhar
+        respostasSelecionadas[i] = respostas[i];
+    }
+
+ /*
+  for (int p = 0; p < tamanho; p++) {
+            lcd_1.clear();
+            lcd_1.print("Contagem: ");
+            lcd_1.print(contagemseq);
+
+            // Espera até que um botão seja pressionado
+            while (true) {
+                int botaovd = digitalRead(8);
+                int botaovm = digitalRead(10);
+                
+                if (botaovd == LOW) {
+                    indsequeusuario = 1;
+                    digitalWrite(verde, HIGH);
+                    delay(1000);
+                    digitalWrite(verde, LOW);
+                    memoriausu[b] = indsequeusuario;
+                    contagemseq++;
+                    break; // Sai do loop enquanto para processar a próxima sequência
+                } 
+                else if (botaovm == LOW) {
+                    indsequeusuario = 0;
+                    digitalWrite(vermelho, HIGH);
+                    delay(1000);
+                    digitalWrite(vermelho, LOW);
+                    memoriausu[b] = indsequeusuario;
+                    contagemseq++;
+                    break; // Sai do loop enquanto para processar a próxima sequência
+             }
+      } 
+  */
+   
+}
